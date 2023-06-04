363fdbafe9a25c2b719db1742617418f9b17a5fe
userid007 <shindeakshay7499@gmail.com>
2023-06-05 00:56:19 +0530

    vercel fuction support added

--- Express/Posts/src/app.js
+++ Express/Posts/src/app.js
@@ -1,23 +1,27 @@
 import express from "express";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
-import YAML from "yaml";
-import fs from "fs";
 import axios from "axios";
+import { swaggerDocument } from "./config/swagger.js";
 
-const swaggerDocument = YAML.parse(fs.readFileSync("./swagger.yaml", "utf8"));
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    customCssUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.min.css",
+  })
+);
 
 app.get("/", async (_req, res) => {
   res.send("Hello there - API");
 });
-
 
 app.get("/post", async (_req, res) => {
   const data = await axios.get("https://jsonplaceholder.typicode.com/posts");
---
+++ Express/Posts/src/config/swagger.js
@@ -0,0 +1,37 @@
+export const swaggerDocument = {
+  openapi: "3.0.0",
+  info: {
+    title: "Ecomm App",
+    description: "REST API Documentation",
+    version: "1.0.0",
+  },
+  servers: [
+    {
+      url: "https://posts-userid007.vercel.app/",
+      description: "Dev Environment",
+    },
+    {
+      url: "http://localhost:3000/",
+      description: "Local Environment",
+    },
+  ],
+  paths: {
+    "/post": {
+      get: {
+        summary: "Post",
+        tags: ["Post"],
+        responses: {
+          200: {
+            description: "Returns 20 posts",
+          },
+          404: {
+            description: "Bad request",
+          },
+          500: {
+            description: "Internal Server Error",
+          },
+        },
+      },
+    },
+  },
+};
